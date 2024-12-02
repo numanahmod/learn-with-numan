21ca8ca64ec8311fba7dde63cf0754204d7531a0
numanahmod <numanahmod96@gmail.com>
2024-12-03 00:47:53 +0600

    added pdf link

--- src/app/ssc/page.jsx
+++ src/app/ssc/page.jsx
@@ -8,127 +8,127 @@ const SSCGrammarPage = () => {
     { 
       title: "Parts of Speech", 
       videoUrl: "/tag.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1mrU3UNVBkHapi805oH_kptOjzTPsn2UW/view?usp=drive_link" 
     },
     { 
       title: "Sentence", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1s01L39uM-rA7LcG6RxjTaE0Mm42N_tSW/view?usp=drive_link" 
     },
     { 
       title: "Tense", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1fSprFQ3mwEpWYpW2pylS-VkZhsQwcV0J/view?usp=drive_link" 
     },
     { 
       title: "Preposition", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1ntCO_4Jp3hmZI-3Hk3qrxc8yKGqKm1CM/view?usp=drive_link" 
     },
     { 
       title: "Preposition-2", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1oap4_eh_wWm24bMUjkSSu5rdZu-p-ttJ/view?usp=drive_link" 
     },
     { 
       title: "01. With Clue", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1GUr9icjGHO9k9uffs6-1ZJoOVQrco2bN/view?usp=drive_link" 
     },
     { 
       title: "02. Without Clue", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1IIy7nw_cApsnuRz8FAbkq3lbWolf9LK_/view?usp=drive_link" 
     },
     { 
       title: "03. Substitution Table", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1A2TVt7cYGKJSflyfWtfs5NwcLADIKok8/view?usp=drive_link" 
     },
     { 
       title: "04. Right Forms of Verbs", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1zOCGCo0NUgOndo-_XczcIyGKtFwEJXsi/view?usp=drive_link" 
     },
     { 
       title: "05. Narration", 
       videoUrl: "/tag.mp4",  
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/10GeRGryeBAIYKfZOtIEoC6TKWQ1-V8_q/view?usp=drive_link" 
     },
     { 
       title: "06. Changing Sentence-1", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1WDGMv24iMWxveopo7k-qbYbJHJ0Fb2Sc/view?usp=drive_link" 
     },
     { 
       title: "06.2 Changing Sentence-2", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1S5vZs6lfBHnWr5wIe3H6kSthMqbzgmjR/view?usp=drive_link" 
     },
     { 
       title: "07. Completing Sentence", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1G8OJA9Za3Dan1b6radujwzgHCZSGVra8/view?usp=drive_link" 
     },
     { 
       title: "08. Suffix-Prefix", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1T5FD9k9_T9b3HEqCORTOy7b1KiJrBpLG/view?usp=drive_link" 
     },
     { 
       title: "09. Tag Questions", 
       videoUrl: "/tag.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1O4cSbEF0fBcWup-jzGB92N93oj5eXIUW/view?usp=drive_link" 
     },
     { 
       title: "10. Sentence Connector", 
       videoUrl: "/videos/voice.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1H7As72wCd3zlJ3nRlXS-iUAlpQAVcsEx/view?usp=drive_link" 
     },
     { 
       title: "11. Capital letter and Punctuation", 
       videoUrl: "", 
-      pdfUrl: "" 
+      pdfUrl: "https://drive.google.com/file/d/1ywpEgT3DLeEhhohk_twXoMWZaL9AD9CD/view?usp=drive_link" 
     },
     { 
       title: "12. CV", 
       videoUrl: "/videos/parts_of_speech.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1ZdZRyEl-ptDkzeZsnUPmnPRjDqXJXMss/view?usp=drive_link" 
     },
     { 
       title: "13. Application", 
       videoUrl: "/videos/prepositions.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1ebQDZ2DhR8T25EDBx-kMV4M6jMtDBWmp/view?usp=drive_link" 
     },
     { 
       title: "14. Paragraph", 
       videoUrl: "", 
-      pdfUrl: "" 
+      pdfUrl: "https://drive.google.com/file/d/1-nkklzR4iafq6-LPVH9egMoItrMmVTSj/view?usp=drive_link" 
     },
     { 
       title: "15. Composition", 
       videoUrl: "/videos/sentence_structure.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1OGYOrx4BSzUMFFnkJ7Tc8qqNUjbcx1RQ/view?usp=drive_link" 
     },
     { 
       title: "16. Importance Verb List", 
       videoUrl: "/videos/modals.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1cGL05YPcktRRRJxyACniv1gCZNHP88eM/view?usp=drive_link" 
     },
     { 
       title: "17. Dialogue", 
       videoUrl: "/videos/conjunctions.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/14OSLddob53WJUDSipqKVnz8tR9q9JM6r/view?usp=drive_link" 
     },
     { 
       title: "18. Graph-chart", 
       videoUrl: "/videos/conjunctions.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1SBQTME03VORz8iuonxCtxWfMwLnYhgsb/view?usp=drive_link" 
     },
     { 
       title: "19. Use of Have", 
       videoUrl: "/videos/conjunctions.mp4", 
-      pdfUrl: "https://drive.google.com/file/d/1dhltEIzRMQJdrdAiMRD3gWpIiRBy42E5/view?usp=sharing" 
+      pdfUrl: "https://drive.google.com/file/d/1xrOkPFS9B7FoNPcwLtzIQi1cEDDHFjEh/view?usp=drive_link" 
     },
   ];
 
